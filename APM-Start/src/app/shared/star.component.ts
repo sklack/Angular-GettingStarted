5c9a53fd4324cf485ac17152c74cc55a9913d10d
jenanh <jenan.hong@sunbirddcim.com>
2021-03-28 19:03:53 +0800

    Service and Dependency Injection.

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,20 +1,20 @@
 import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 
 @Component({
-    selector: 'pm-star',
-    templateUrl: './star.component.html',
-    styleUrls: ['./star.component.css']
+	selector: 'pm-star',
+	templateUrl: './star.component.html',
+	styleUrls: ['./star.component.css']
 })
 
 export class StarCompnent implements OnChanges {
-    @Input() rating: number = 4;
-    starWidth: number = 75;
-    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>()
-    ngOnChanges(): void {
-        this.starWidth = this.rating * 75/5
-    }
+	@Input() rating: number = 4;
+	starWidth: number = 75;
+	@Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>()
+	ngOnChanges(): void {
+		this.starWidth = this.rating * 75 / 5
+	}
 
-    onClick(): void {
-        this.ratingClicked.emit(`The rating is ${this.rating}`)
-    }
+	onClick(): void {
+		this.ratingClicked.emit(`The rating is ${this.rating}`)
+	}
 }
